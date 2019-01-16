cea9607fb3327c7be8333b36ee445030d12351ab
David Cai <davidcai1993@yahoo.com>
2019-01-16 15:15:12 +0800

    test: add pushToUser(s) tests

--- test/api/user.test.js
+++ test/api/user.test.js
@@ -6,19 +6,32 @@ const SocketClient = require('socket.io-client')
 const ilog = require('ilog')
 
 describe('api - user', function () {
-  let userId
+  let userId, userId2, userId3
   before(function (callback) {
     userId = Math.random().toString(36).substring(7)
+    userId2 = Math.random().toString(36).substring(7)
+    userId3 = Math.random().toString(36).substring(7)
 
-    const client = SocketClient('http://localhost:9001', {
-      query: { userId },
-      path: '/ws'
-    })
+    const userIds = [userId, userId2, userId3]
 
-    client.on('connect', function () {
-      ilog.debug({ class: 'client-connect', message: client.connected })
-      callback()
-    })
+    let connected = 0
+
+    for (const id of userIds) {
+      const client = SocketClient('http://localhost:9001', {
+        query: { userId: id },
+        path: '/ws'
+      })
+
+      client.on('connect', function () {
+        ilog.debug({
+          class: 'client-connect',
+          message: client.connected,
+          userId: id
+        })
+
+        if (++connected === userIds.length) callback()
+      })
+    }
   })
 
   describe('isOnline', function () {
@@ -28,6 +41,45 @@ describe('api - user', function () {
         .expect(200)
 
       assert.strictEqual(body.isOnline, true)
+    })
+
+    it('Offline User', async function () {
+      let { body } = await request
+        .get(`/users/${Math.random().toString(36).substring(7)}/online`)
+        .expect(200)
+
+      assert.strictEqual(body.isOnline, false)
+    })
+  })
+
+  describe('pushToUser', function () {
+    it('Push to online user', async function () {
+      let { body } = await request
+        .post(`/users/${userId}/push`)
+        .send({ a: 'aa', b: 'bb' })
+        .expect(200)
+
+      assert.strictEqual(body.count, 1)
+    })
+
+    it('Push to offline user', async function () {
+      let { body } = await request
+        .post(`/users/${Math.random().toString(36).substring(7)}/push`)
+        .send({ a: 'aa', b: 'bb' })
+        .expect(200)
+
+      assert.strictEqual(body.count, 0)
+    })
+  })
+
+  describe('pushToUsers', function () {
+    it('Push to online users', async function () {
+      let { body } = await request
+        .post(`/users/push`)
+        .send({ a: 'aa', b: 'bb', userIds: [userId, userId3] })
+        .expect(200)
+
+      assert.strictEqual(body.count, 2)
     })
   })
 })
