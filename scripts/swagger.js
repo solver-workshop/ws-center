ddfea63c2f04f7e3828127c2d6764666e0a4265f
David Cai <davidcai1993@yahoo.com>
2019-01-16 16:58:53 +0800

    feat: update docs

---
+++ scripts/swagger.js
@@ -0,0 +1,30 @@
+'use strict'
+const path = require('path')
+const fs = require('fs')
+const swaggerJSDoc = require('swagger-jsdoc')
+const glob = require('glob')
+const pkg = require('../package')
+
+const apis = glob.sync(path.join(__dirname, '../lib/api/*.js'))
+
+if (!fs.existsSync(path.join(__dirname, '../public'))) {
+  fs.mkdirSync(path.join(__dirname, '../public'))
+}
+const outputPath = path.join(__dirname, '../public/swagger.json')
+
+const options = {
+  definition: {
+    info: {
+      title: pkg.name,
+      version: pkg.version,
+      description: pkg.description
+    }
+  },
+  apis
+}
+
+const swaggerSpec = swaggerJSDoc(options)
+
+fs.writeFileSync(outputPath, JSON.stringify(swaggerSpec))
+
+console.log(`=> ${outputPath}`)
